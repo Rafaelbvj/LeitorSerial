004f25948c68c470ab1d872b7cc761cfe730c63d
Rafael Costa <Rafaelbvj@gmail.com>
2020-02-19 21:18:01 -0300

    Adjusting 2.0
    Setting path order to the files

---
+++ LeitorSerial/LeitorSerial/include/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Arquivo de inclusão gerado pelo Microsoft Visual C++.
+// Usado por LeitorSerial.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FORMVIEW                    101
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT1             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_OPOK                        1002
+#define IDC_EDIT2                       1004
+#define IDC_BAUDRATE                    1004
+#define ID_ARQUIVO                      32771
+#define ID_FONTE                        32776
+#define ID_FERRAMENTAS_PORTAS           32777
+#define ID_ARQUIVO_SALVAR               32782
+#define ID_PREFERENCIAS_OP32783         32783
+#define ID_OP                           32784
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ LeitorSerial/LeitorSerial/src/Graficos.cpp
@@ -0,0 +1,87 @@
+
+#include "Graficos.h"
+Graficos* Graficos::gr = 0;
+Graficos& Graficos::GetInstanceGNUPlot() {
+	if (gr == 0) {
+		gr = new Graficos;
+	}
+	return *gr;
+}
+
+bool Graficos::StartGNUPlotProgram(string const& strcmd) {
+	GnuFilePath.append(strcmd);
+
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sat, 0)) {
+		
+		return false;
+	}
+	st.hStdError = hWritePipe;
+	st.hStdOutput = hWritePipe;
+	st.hStdInput = hReadPipe;
+	st.dwFlags = STARTF_USESTDHANDLES;
+	if (!CreateProcessA(GnuFilePath.c_str(),0,0,0,TRUE,NORMAL_PRIORITY_CLASS|CREATE_NO_WINDOW,0,0,&st,&pi)) {
+		return false;
+	}
+	return true;
+}
+bool Graficos::StartGNUPlotProgram() {
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sat, 0)) {
+		return false;
+	}
+	st.hStdError = hWritePipe;
+	st.hStdOutput = hWritePipe;
+	st.hStdInput = hReadPipe;
+	st.dwFlags = STARTF_USESTDHANDLES;
+	if (!CreateProcessA(GnuFilePath.c_str(), 0, 0, 0, TRUE, NORMAL_PRIORITY_CLASS | CREATE_NO_WINDOW, 0, 0, &st, &pi)) {
+		return false;
+	}
+	return true;
+}
+
+
+int Graficos::SetScriptFile(string const& path) {
+	char* f = NULL;
+	long size;
+	if (fileExist(path)) {
+		fopen_s(&script, path.c_str(), "rb");
+		if (script == NULL){
+			return -2;
+		}
+		fseek(script, 0, SEEK_END);
+		size = ftell(script);
+		rewind(script);
+		
+		f = (char*)malloc(sizeof(char) * size);
+		if (f) {
+			if (fread(f, sizeof(char), size, script) == size) {
+				scriptcode = f;
+				fclose(script);
+				free(f);
+				return size;
+			}
+		}
+	}
+	free(f);
+	return -1;
+
+}
+
+Graficos::Graficos() {
+	ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+	ZeroMemory(&st, sizeof(STARTUPINFO));
+	ZeroMemory(&sat, sizeof(SECURITY_ATTRIBUTES));
+	sat.bInheritHandle = TRUE;
+	sat.lpSecurityDescriptor = NULL;
+	sat.nLength = sizeof(SECURITY_ATTRIBUTES);
+	char* pf = NULL;
+	_dupenv_s(&pf, NULL, "PROGRAMFILES(X86)");
+ 
+	if (pf) {
+		GnuFilePath.append(pf);
+		GnuFilePath.append("\\gnuplot\\bin\\gnuplot.exe");
+	}
+	free(pf);
+}
+Graficos::~Graficos() {
+	
+}
